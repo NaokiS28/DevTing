6a650f2d5952a2c9abcc15d13e0b2fc98f0163e5
NaokiS28 <biz@nsaito.co.uk>
2025-09-12 16:29:07 +0100

    Initial Commit

---
+++ DevTing/DevTingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevTingApp.swift
+//  DevTing
+//
+//  Created by Naoki Saito on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DevTingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
